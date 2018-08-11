7ac4cd5487343074e23b75afa008d5b157f4b735
Ruben Verborgh <ruben@verborgh.org>
2018-08-11 16:51:40 +0200

    Allow logging in without popup.

--- demo/components/App.js
+++ demo/components/App.js
@@ -8,28 +8,32 @@ import PersonalInfo from './PersonalInfo'
 import Footer from './Footer'
 
 import type { Session } from '../../src/session'
-import { popupLogin, logout, currentSession } from '../../src/'
+import { login, popupLogin, logout, currentSession } from '../../src/'
 
 export default class App extends React.Component<Object, Object> {
   state: { session: ?Session } = { session: null }
 
-  saveCredentials = (session: Session): void => {
+  constructor(props: {}) {
+    super(props)
+    currentSession().then(session => this.setState({ session }))
+  }
+
+  async login() {
+    const idp = window.prompt('Please enter your identity provider')
+    if (idp) {
+      const session = await login(idp)
+      this.setState({ session })
+    }
+  }
+
+  async loginPopup() {
+    const session = await popupLogin({ popupUri: process.env.POPUP_URI })
     this.setState({ session })
   }
 
-  onClickLogIn = () =>
-    popupLogin({
-      popupUri: process.env.POPUP_URI
-    }).then(this.saveCredentials)
-
-  onClickLogOut = () =>
-    logout().then(() => {
-      this.setState({ session: null })
-    })
-
-  constructor(props: {}) {
-    super(props)
-    currentSession().then(this.saveCredentials)
+  async logout() {
+    await logout()
+    this.setState({ session: null })
   }
 
   render() {
@@ -38,8 +42,9 @@ export default class App extends React.Component<Object, Object> {
       <div>
         <Nav
           loggedIn={loggedIn}
-          onClickLogIn={this.onClickLogIn}
-          onClickLogOut={this.onClickLogOut}
+          onClickLogIn={() => this.login()}
+          onClickLogInPopup={() => this.loginPopup()}
+          onClickLogOut={() => this.logout()}
         />
         <Copy loggedIn={loggedIn} />
         <PersonalInfo session={this.state.session} />
--- demo/components/Nav.js
+++ demo/components/Nav.js
@@ -4,16 +4,27 @@ import React from 'react'
 type propTypes = {
   loggedIn: boolean,
   onClickLogIn: (event: Event) => any,
+  onClickLogInPopup: (event: Event) => any,
   onClickLogOut: (event: Event) => any
 }
 
-const Nav = ({ loggedIn, onClickLogIn, onClickLogOut }: propTypes) => (
+const Nav = ({
+  loggedIn,
+  onClickLogIn,
+  onClickLogInPopup,
+  onClickLogOut
+}: propTypes) => (
   <nav>
     <h1>Solid Auth Client Demo</h1>
     {loggedIn ? (
       <button onClick={onClickLogOut}>Log out</button>
     ) : (
-      <button onClick={onClickLogIn}>Log in</button>
+      [
+        <button onClick={onClickLogIn}>Log in</button>,
+        <button onClick={onClickLogInPopup}>
+          Log in <em>(popup)</em>
+        </button>
+      ]
     )}
   </nav>
 )
