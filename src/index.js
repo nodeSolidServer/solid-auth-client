6c1209b3051aca8719ee66797065cddd1c4b1933
Ruben Verborgh <ruben@verborgh.org>
2018-08-23 20:54:26 -0400

    Bind methods to instance.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import SolidAuthClient from './solid-auth-client'
 // Export a singleton instance of SolidAuthClient
 const instance = new SolidAuthClient()
 export default instance
+
+// Bind methods to instance, so they can be invoked as regular functions
+// (e.g., to pass around the fetch function)
+Object.getOwnPropertyNames(SolidAuthClient.prototype).forEach(property => {
+  const value = instance[property]
+  if (typeof value === 'function') {
+    instance[property] = value.bind(instance)
+  }
+})
