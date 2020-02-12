85bb59b43f71373c9ca404890d96d576aa989cf8
Marc Fornós <frozen.pomelo@gmail.com>
2020-02-12 19:36:30 +0100

    Update @solid/oidc-rp version to 0.10.0

--- src/webid-oidc.js
+++ src/webid-oidc.js
@@ -2,7 +2,7 @@
 /* global Response */
 import * as authorization from 'auth-header'
 import RelyingParty from '@solid/oidc-rp'
-import PoPToken from '@solid/oidc-rp/lib/PoPToken'
+import PoPToken from '@solid/oidc-rp/src/PoPToken'
 
 import type { loginOptions } from './solid-auth-client'
 import { currentUrl, navigateTo, toUrlString } from './url-util'
@@ -153,11 +153,14 @@ function registerRp(idp: string, opts: loginOptions): Promise<RelyingParty> {
     ...supplementaryOptions
   }
 
+  // Note that overrides @solid/oidc-rp/RelyingParty defaults (i.e. not merged)
   const options = {
     defaults: {
       authenticate: {
         redirect_uri: callbackUri,
-        response_type: responseType
+        response_type: responseType,
+        display: 'page',
+        scope: ['openid']
       }
     },
     store: storage
